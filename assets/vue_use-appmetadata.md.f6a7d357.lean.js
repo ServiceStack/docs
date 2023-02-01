093e9dfffc59b1cf06d35f84b5bde876b1ed27ca
GitHub Action <action@github.com>
2023-02-01 11:14:31 +0000

    deploy: 37ddce042898c87143381d6eeb4aa87359a838fc

---
+++ assets/vue_use-appmetadata.md.f6a7d357.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./app.4a3f4073.js";const u='{"title":"useAppMetadata","description":"","frontmatter":{"title":"useAppMetadata"},"headers":[],"relativePath":"vue/use-appmetadata.md"}',p={};function r(s,o,c,n,d,_){return t(),a("div")}var m=e(p,[["render",r]]);export{u as __pageData,m as default};
