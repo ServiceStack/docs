f245ad6e0de51a5f9a6ad46ef8fd0021734cee9b
Demis Bellot <demis.bellot@gmail.com>
2023-06-03 12:07:32 +0800

    Add new FileLayout component

---
+++ docs/src/components/FileLayout.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <FileLayoutFile v-for="(contents,label) in files" :label="`${label}`" :contents="contents" />
+</div>
+</template>
+
+<script setup lang="ts">
+import FileLayoutFile from './FileLayoutFile.vue'
+
+/**
+ files: {
+        emails: { 
+            layouts:  { _: ['basic.html','empty.html','marketing.html'] },
+            partials: { _: ['button-centered.html','divider.html','image-centered.html','section.html','title.html'] },
+            vars:     { _: ['info.txt','urls.txt'] },
+            _:        ['empty.html','newsletter-welcome.html','newsletter.html','verify-email.html'] 
+        }
+    }
+ */
+defineProps<{
+    files: any
+}>()
+</script>
