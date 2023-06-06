3f3e9d8cdf9705c9b8df7d2b91bedf17d9c99e61
GitHub Action <action@github.com>
2023-06-06 12:34:54 +0000

    deploy: cfffd7046e00882e7ef39cefd86a6c4bff3e822a

--- assets/chunks/ApiReference.25bbe383.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as a,c as n,d as s,p as r,b as c,A as l,B as m}from"../app.84d05c32.js";const p={class:"mt-8 mb-4 text-2xl font-semibold text-gray-900 dark:text-gray-50 flex justify-between"},f=o({__name:"ApiReference",props:{component:null},setup(e){return(t,d)=>(a(),n("h2",p,[s("div",null,[r(t.$slots,"default",{},()=>[c(l(e.component),1)])]),m("",!0)]))}});export{f as _};
